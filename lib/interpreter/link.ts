297742c9c9aeceb57cbe205e47347101a75db703
Bartosz Krupa <krupa.bartosz@gmail.com>
2019-10-31 22:51:20 +0100

    Don't use additional div

--- lib/interpreter/link.ts
+++ lib/interpreter/link.ts
@@ -1,19 +1,14 @@
 import { createDOMElementFromURL, vanillinEval, VanillinEvaluationConfig } from "../vanillin-0";
 
-export function Link({ element }, c, cerr, env, config: VanillinEvaluationConfig) {
-  const {
-    window: { document }
-  } = config;
-
+export function Link({ element }: { element: HTMLElement }, c, cerr, env, config: VanillinEvaluationConfig) {
   let href;
   if (element.getAttribute("rel") === "include" && (href = element.getAttribute("href"))) {
     createDOMElementFromURL(
       href,
       function(inclusion) {
-        const container = document.createElement("div");
-        container.appendChild(inclusion);
-        element.insertAdjacentElement("afterend", container);
-        vanillinEval(container, c, cerr, env, config);
+        const inclusionChildren = Array.from(inclusion.childNodes);
+        element.parentNode!.insertBefore(inclusion, element);
+        vanillinEval(inclusionChildren, c, cerr, env, config);
       },
       cerr,
       env,
